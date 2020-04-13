ab784b693248e78bc3aa397ae27321809de62b56
Tomasz Steć <tomeczekstecc@gmail.com>
2020-04-13 10:17:43 +0200

    films backend & improve pdf

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/api/films", require("./router/films"));
 app.use("/api/upload", require("./router/upload"));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../index.html'), function (err) {
+  res.sendFile(path.join(__dirname, './index.html'), function (err) {
     if (err) {
       res.status(500).send(err);
     }
